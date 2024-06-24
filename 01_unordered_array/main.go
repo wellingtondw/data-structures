32ff5380d2ca658d3aa88995370149d4db1e7bb2
Wellington Lima <wellington.map@hotmail.com>
2024-06-24 09:59:20 -0300

    feat: create unordered array search function

--- 01_unordered_array/main.go
+++ 01_unordered_array/main.go
@@ -26,6 +26,7 @@ func (ua *UnorderedArray) Print() {
 	}
 }
 
+// O(1)
 func (ua *UnorderedArray) Insert(value int) {
 	if ua.lastPosition == ua.capacity-1 {
 		fmt.Println("Maximum capacity reached")
@@ -35,14 +36,32 @@ func (ua *UnorderedArray) Insert(value int) {
 	}
 }
 
+// O(n)
+func (ua *UnorderedArray) Search(value int) int {
+	for i := 0; i <= ua.lastPosition; i++ {
+		if ua.values[i] == value {
+			return i
+		}
+	}
+
+	return -1
+}
+
 func main() {
 	ua := NewUnorderedArray(10)
 
+	fmt.Println("Insert")
 	ua.Insert(3)
 	ua.Insert(1)
 	ua.Insert(4)
-	ua.Insert(1)
 	ua.Insert(5)
+	ua.Insert(8)
 
 	ua.Print()
+
+	fmt.Println("Search")
+	fmt.Println("Index of 5:", ua.Search(5))
+	fmt.Println("Index of 1:", ua.Search(1))
+	fmt.Println("Index of 4:", ua.Search(4))
+
 }
